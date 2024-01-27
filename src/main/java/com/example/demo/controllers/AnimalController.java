74c86cc6c2177bbec30486bd1fd1c467dde33d72
frankbigshuai <yuntzeng@iu.edu>
2024-01-26 20:33:58 -0500

    lab03

--- src/main/java/com/example/demo/controllers/AnimalController.java
+++ src/main/java/com/example/demo/controllers/AnimalController.java
@@ -1,12 +1,60 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.AnimalData;
 import com.example.demo.repository.AnimalRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping
-private AnimalRepository animalRepository;
-public AnimalController(AnimalRepository animalRepository) {
-    this.animalRepository=animalRepository;
+@RequestMapping("/animals")
+public class AnimalController {
+    private AnimalRepository animalRepository;
+
+    public AnimalController(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    @PostMapping
+    public boolean add(@RequestBody AnimalData data) {
+        try {
+            return animalRepository.add(data);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @GetMapping
+    public List<AnimalData> findAll() {
+        try {
+            return animalRepository.findAll();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @GetMapping
+    public List<AnimalData> find(String name, String picture, String location) throws IOException
+    {
+        List<AnimalData> result = new ArrayList<>();
+        List<AnimalData> allAnimals= findAll();
+        for(AnimalData animal: allAnimals)
+        {
+            if ((name==null||animal.getName().equals(name))&&
+            (picture==null||animal.getPicture().equals(picture))&&
+                    (location==null||animal.getLocation().equals(location)))
+            {
+                result.add(animal);
+            }
+
+        }
+
+        return result;
+    }
+
 }
+
+
+
--- src/main/java/com/example/demo/repository/AnimalRepository.java
+++ src/main/java/com/example/demo/repository/AnimalRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.AnimalData;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,6 +11,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AnimalRepository {
@@ -29,5 +33,34 @@ public class AnimalRepository {
                 "," + animalData.getLocation();
         appendToFile(path,data+ NEW_Line);
         return true;
+    }
+    public List<AnimalData> findAll() throws IOException{
+        List<AnimalData> result = new ArrayList<>();
+        Path path = Paths.get(DATABASE_Name);
+        List<String> data = Files.readAllLines(path);
+        for (String line : data){
+            String[] words = line.split(",");
+            AnimalData a = new AnimalData();
+            a.setName(words[0]);
+            a.setPicture(words[1]);
+            a.setLocation(words[2]+","+words[3]);
+            result.add(a);
+        }
+        return result;
+    }
+
+    @GetMapping("/search")
+    public List<AnimalData> Search (@RequestParam String name,
+                                    @RequestParam String picture,
+                                    @RequestParam String location)
+    {
+        try{
+            System.out.println(name);
+            System.out.println(picture);
+            System.out.println(location);
+            return animalRepository.find(name,picture,location);
+        } catch(IOException e){
+            return null;
+        }
     }
 }
